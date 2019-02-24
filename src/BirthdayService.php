b8f6730172aa12dd5ab0a32fe3664a671f64de4b
tλeUniC <theunic@gmail.com>
2019-02-24 21:18:58 +0100

    10 - Introduced BirthdayGreet concept

--- src/BirthdayService.php
+++ src/BirthdayService.php
@@ -28,7 +28,65 @@ final class BirthdayService
             $recipient = $employee->getEmail();
             $body = sprintf('Happy Birthday, dear %s!', $employee->getFirstName());
             $subject = 'Happy Birthday!';
-            $this->sendMessage($smtpHost, $smtpPort, 'sender@here.com', $subject, $body, $recipient);
+            
+            $birthdayGreet = new class($employee) {
+                /**
+                 * @var string
+                 */
+                private $from;
+                
+                /**
+                 * @var string
+                 */
+                private $title;
+                
+                /**
+                 * @var string
+                 */
+                private $message;
+                
+                /**
+                 * @var string
+                 */
+                private $to;
+
+                public function __construct(Employee $employee)
+                {
+                    $this->from = 'sender@here.com';
+                    $this->title = 'Happy Birthday!';
+                    $this->message = sprintf('Happy Birthday, dear %s!', $employee->getFirstName());
+                    $this->to = $employee->getEmail();
+                }
+
+                public function from(): string
+                {
+                    return $this->from;
+                }
+
+                public function title(): string
+                {
+                    return $this->title;
+                }
+
+                public function message(): string
+                {
+                    return $this->message;
+                }
+
+                public function to(): string
+                {
+                    return $this->to;
+                }
+            };
+            
+            $this->sendMessage(
+                $smtpHost,
+                $smtpPort, 
+                $birthdayGreet->from(),
+                $birthdayGreet->title(),
+                $birthdayGreet->message(),
+                $birthdayGreet->to()
+            );
         }
     }
 
