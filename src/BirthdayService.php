f138708d8703a1406cea0f74bf85c38468673ab0
tλeUniC <theunic@gmail.com>
2019-02-24 21:45:22 +0100

    14 - Introduced BirthdayGreetSender concept

--- src/BirthdayService.php
+++ src/BirthdayService.php
@@ -26,8 +26,47 @@ final class BirthdayService
         
         foreach ($employees as $employee) {
             $birthdayGreet = new BirthdayGreet($employee);
+            $birthdayGreetSender = new class($smtpHost, $smtpPort) {
+                /**
+                 * @var string
+                 */
+                private $smtpHost;
+
+                /**
+                 * @var int
+                 */
+                private $smtpPort;
+
+                public function __construct(string $smtpHost, int $smtpPort)
+                {
+                    $this->smtpHost = $smtpHost;
+                    $this->smtpPort = $smtpPort;
+                }
+
+                public function send($smtpHost, $smtpPort, BirthdayGreet $birthdayGreet): void
+                {
+                    $this->smtpHost = $smtpHost;
+                    $this->smtpPort = $smtpPort;
+                    
+                    // Create a mailer
+                    $mailer = new Swift_Mailer(
+                        new Swift_SmtpTransport($this->smtpHost, $this->smtpPort)
+                    );
+
+                    // Construct the message
+                    $msg = new Swift_Message($birthdayGreet->title());
+                    $msg
+                        ->setFrom($birthdayGreet->from())
+                        ->setTo([$birthdayGreet->to()])
+                        ->setBody($birthdayGreet->message())
+                    ;
+
+                    // Send the message
+                    $mailer->send($msg);
+                }
+            };
             
-            $this->sendMessage($smtpHost, $smtpPort, $birthdayGreet);
+            $birthdayGreetSender->send($smtpHost, $smtpPort, $birthdayGreet);
         }
     }
 
