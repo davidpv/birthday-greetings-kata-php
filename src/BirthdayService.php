f9826cba6e45122a488ae38da44dea458cb9fa32
tλeUniC <theunic@gmail.com>
2019-02-24 19:23:50 +0100

    02 - Introduction of Employee Repository concept

--- src/BirthdayService.php
+++ src/BirthdayService.php
@@ -23,6 +23,36 @@ final class BirthdayService
                 $employees[] = $employee;
             }
         }
+
+        $employeeRepository = new class($fileName) {
+            /**
+             * @var string
+             */
+            private $fileName;
+            
+            public function __construct(string $fileName)
+            {
+                $this->fileName = $fileName;
+            }
+
+            public function byBirthday(XDate $xDate): array
+            {
+                $fileHandler = fopen($this->fileName, 'rb');
+                fgetcsv($fileHandler);
+                $employees = [];
+
+                while ($employeeData = fgetcsv($fileHandler, null)) {
+                    $employeeData = array_map('trim', $employeeData);
+                    $employee = new Employee($employeeData[1], $employeeData[0], $employeeData[2], $employeeData[3]);
+                    if ($employee->isBirthday($xDate)) {
+                        $employees[] = $employee;
+                    }
+                }
+                
+                return $employees;
+            }
+        };
+        $employees = $employeeRepository->byBirthday($xDate);
         
         foreach ($employees as $employee) {
             $recipient = $employee->getEmail();
