0c3c2f00abfccb6ca709a69c67e9f574dedb909d
tλeUniC <theunic@gmail.com>
2019-02-24 21:49:30 +0100

    16 - Clean up the kitchen: removed unused parameters from method

--- src/BirthdayService.php
+++ src/BirthdayService.php
@@ -14,6 +14,7 @@ final class BirthdayService
      * @var EmployeeRepository
      */
     private $employeeRepository;
+    private $birthdayGreetSender;
 
     public function __construct(EmployeeRepository $employeeRepository)
     {
@@ -23,50 +24,47 @@ final class BirthdayService
     public function sendGreetings(XDate $xDate, $smtpHost, $smtpPort): void
     {
         $employees = $this->employeeRepository->byBirthday($xDate);
+
+        $this->birthdayGreetSender = new class($smtpHost, $smtpPort)
+        {
+            /**
+             * @var string
+             */
+            private $smtpHost;
+
+            /**
+             * @var int
+             */
+            private $smtpPort;
+
+            public function __construct(string $smtpHost, int $smtpPort)
+            {
+                $this->smtpHost = $smtpHost;
+                $this->smtpPort = $smtpPort;
+            }
+
+            public function send(BirthdayGreet $birthdayGreet): void
+            {
+                // Create a mailer
+                $mailer = new Swift_Mailer(
+                    new Swift_SmtpTransport($this->smtpHost, $this->smtpPort)
+                );
+
+                // Construct the message
+                $msg = new Swift_Message($birthdayGreet->title());
+                $msg
+                    ->setFrom($birthdayGreet->from())
+                    ->setTo([$birthdayGreet->to()])
+                    ->setBody($birthdayGreet->message());
+
+                // Send the message
+                $mailer->send($msg);
+            }
+        };
         
         foreach ($employees as $employee) {
             $birthdayGreet = new BirthdayGreet($employee);
-            $birthdayGreetSender = new class($smtpHost, $smtpPort) {
-                /**
-                 * @var string
-                 */
-                private $smtpHost;
-
-                /**
-                 * @var int
-                 */
-                private $smtpPort;
-
-                public function __construct(string $smtpHost, int $smtpPort)
-                {
-                    $this->smtpHost = $smtpHost;
-                    $this->smtpPort = $smtpPort;
-                }
-
-                public function send($smtpHost, $smtpPort, BirthdayGreet $birthdayGreet): void
-                {
-                    $this->smtpHost = $smtpHost;
-                    $this->smtpPort = $smtpPort;
-                    
-                    // Create a mailer
-                    $mailer = new Swift_Mailer(
-                        new Swift_SmtpTransport($this->smtpHost, $this->smtpPort)
-                    );
-
-                    // Construct the message
-                    $msg = new Swift_Message($birthdayGreet->title());
-                    $msg
-                        ->setFrom($birthdayGreet->from())
-                        ->setTo([$birthdayGreet->to()])
-                        ->setBody($birthdayGreet->message())
-                    ;
-
-                    // Send the message
-                    $mailer->send($msg);
-                }
-            };
-            
-            $birthdayGreetSender->send($smtpHost, $smtpPort, $birthdayGreet);
+            $this->birthdayGreetSender->send($birthdayGreet);
         }
     }
 }
